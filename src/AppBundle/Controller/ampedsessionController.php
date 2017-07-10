f22f5fe46db3e1cb0f943a09d69ddeb7031bd008
Sagar Limaye <sagarl3232@hotmail.com>
2017-07-10 11:44:38 -0500

    Initalized property in Amped Session

---
+++ src/AppBundle/Controller/ampedsessionController.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Controller;
+use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+/**
+ * Description of AmpedSessionAdmin
+ *
+ * @author sagar
+ */
+class ampedsessionController extends BaseAdminController {
+    //put your code here
+    
+    protected function prePersistEntity($entity)
+    {
+        $usr= $this->getUser();
+        
+        if (method_exists($entity, 'setCreatedBy'))
+        {
+            $entity->setCreatedBy($usr);
+        }       
+        parent::prePersistEntity($entity);
+    }
+
+    
+}
