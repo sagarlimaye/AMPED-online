084802dcea186d8ba2b92dfa1287175d040cf543
Sagar Limaye <sagarl3232@hotmail.com>
2017-08-09 17:47:47 -0500

    Added entity to store informational pages

---
+++ src/AppBundle/Entity/SimplePage.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * SimplePage
+ */
+class SimplePage
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return SimplePage
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return SimplePage
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    public function __toString() {
+        return $this->getTitle();
+    }
+}
+
