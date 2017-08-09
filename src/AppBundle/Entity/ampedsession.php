6cedfa5ba0d31be391c16449f4b33c4241e4f846
Sagar Limaye <sagarl3232@hotmail.com>
2017-08-09 18:47:26 -0500

    Changed method name

--- src/AppBundle/Entity/ampedsession.php
+++ src/AppBundle/Entity/ampedsession.php
@@ -284,7 +284,7 @@ class ampedsession
     {
         return $this->abm;
     }
-    public function getChangeForm()
+    public function getChangeFormQuestions()
     {
         return $this->changeFormQuestions;
     }
