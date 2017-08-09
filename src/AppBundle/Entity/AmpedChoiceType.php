f8217abdb229edf89e43afdc6828a52eb79aa236
Sagar Limaye <sagarl3232@hotmail.com>
2017-08-09 16:18:53 -0500

    Removed unneeded specialization for choice type field classes

--- src/AppBundle/Entity/AmpedChoiceType.php
+++ src/AppBundle/Entity/AmpedChoiceType.php
@@ -7,8 +7,24 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
  */
 class AmpedChoiceType extends QuestionType
 {
+    private $groupType;
+    private $choiceCount;
+    
     public function getParent() {
         return ChoiceType::class;
+    }
+        
+    
+    public function getGroupType() {
+        return $this->groupType;
+    }
+    public function setGroupType($groupType) {
+        $this->groupType = $groupType;
+        return $this;
+    }    
+    public function getBlockPrefix()
+    {
+        return 'ampedchoicetype';
     }
 }
 
