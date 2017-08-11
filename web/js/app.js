a52435bf64ae63682824f2a0a2b1b6bfa8e963e7
Sagar Limaye <sagarl3232@hotmail.com>
2017-08-11 09:38:25 -0500

    Removed app.js

--- web/js/app.js
+++
@@ -1,9 +0,0 @@
-function thingsInCommon(button_id) {
-    $("button#"+button_id).attr("disabled", true);
-    $("button#"+button_id).children("div.hidden").removeClass("hidden");
-}
-
-function onNextClick() {
-    var fileURL="../Session "
-    window.open("../Session 2/icebreaker.html", "_self");        
-}      
