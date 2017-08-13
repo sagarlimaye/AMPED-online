e6f28782e4bfe010f6f8c3b057c5a94eeb6b7c7e
Sagar Limaye <sagarl3232@hotmail.com>
2017-08-13 12:50:36 -0500

    Added listener to redirect to login page after resetting password

---
+++ src/AppBundle/EventListener/PasswordResettingListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * Listener responsible to change the redirection at the end of the password resetting
+ */
+class PasswordResettingListener implements EventSubscriberInterface {
+    private $router;
+
+    public function __construct(UrlGeneratorInterface $router) {
+        $this->router = $router;
+    }
+
+    public static function getSubscribedEvents() {
+        return [
+            FOSUserEvents::RESETTING_RESET_SUCCESS => 'onPasswordResettingSuccess',
+        ];
+    }
+
+    public function onPasswordResettingSuccess(FormEvent $event) {
+        $url = $this->router->generate('fos_user_security_login');
+        $event->setResponse(new RedirectResponse($url));
+    }
+}
